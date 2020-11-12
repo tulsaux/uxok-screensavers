72dd8466af107689e7919a718cd24442d53b40da
Maxx Crawford <maxx.crawford@gmail.com>
2020-11-12 08:52:14 -0600

    Added countdown page

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+	(function() {
+		// all your code here
+		var countDownDate = new Date("Nov 12, 2020 9:00:00").getTime();
+
+		// Update the count down every 1 second
+		var x = setInterval(function() {
+
+			// Get today's date and time
+			var now = new Date().getTime();
+
+			// Find the distance between now and the count down date
+			var distance = countDownDate - now;
+
+			// Time calculations for days, hours, minutes and seconds
+			// var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			// var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+			// Display the result in the element with id="demo"
+			document.getElementById("timer").innerHTML = "We'll be starting in " + minutes + ":" + seconds;
+
+			// If the count down is finished, write some text
+			if (distance < 0) {
+				clearInterval(x);
+				document.getElementById("timer").innerHTML = "We're starting now!";
+			}
+		}, 1000);
+
+	})();
