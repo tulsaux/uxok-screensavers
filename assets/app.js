714b5a90c21c0eaabcf510cda29a9439ed2f0440
Maxx Crawford <maxx.crawford@gmail.com>
2021-02-05 14:58:20 -0600

    Fix leading zero issue

--- assets/app.js
+++ assets/app.js
@@ -32,6 +32,12 @@
 			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+			// Leading Zero Fix
+			if (seconds.toString().length < 2) {
+				seconds = seconds.toString();
+				seconds = 0 + seconds;
+			}
+
 			// Display the result in the element with id="demo"
 			document.getElementById("timer").innerHTML = wellBeText + minutes + ":" + seconds;
 
